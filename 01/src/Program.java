28b320032398f12446d136ea9fd6e166406b6c98
Carlos Germán <carlos.german.romero@gmail.com>
2021-02-19 20:42:48 +0100

    Probando la subida

---
+++ 01/src/Program.java
@@ -0,0 +1,18 @@
+/**
+ * Project     : Advent of Code
+ * Headline    : Practice programming.
+ *
+ * (C) 2021 Carlos German Romero
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * For more information, see see <http://www.gnu.org/licenses/>
+ */
+
+public class Program {
+   public static void main (String[] args) {
+      System.out.println("Hello world!");
+   }
+}
