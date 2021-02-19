435ac03d6e871df43876d508f22630ac5c4bc425
Carlos Germán <carlos.german.romero@gmail.com>
2021-02-19 22:08:35 +0100

    AOC_2020, ejercicio 1 superado ;-)

--- 01/src/Program.java
+++ 01/src/Program.java
@@ -1,3 +1,7 @@
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
 /**
  * Project     : Advent of Code
  * Headline    : Practice programming.
@@ -12,7 +16,48 @@
  */
 
 public class Program {
-   public static void main (String[] args) {
-      System.out.println("Hello world!");
+   public static void main(String[] args) throws Exception {
+      List<String> l = Files.readAllLines(Paths.get("D:\\src\\practicas\\AOC_2020\\01\\src\\problem.txt"));
+      if (l.size() < 2) {
+         System.out.println("No hay suficientes datos");
+         return;
+      }
+      
+      String result = partOne(l);
+      System.out.println("Part one - solution: " + result);
+      result = partTwo(l);
+      System.out.println("Part two - solution: " + result);
    }
+
+   private static String partOne(List<String> l) {
+      for (int i=0; i < l.size() - 1; i++) {
+         String candidate = l.get(i);
+         long lCandidate = Long.parseLong(candidate);
+         for (int j=i+1; j < l.size(); j++) {
+            long lPair = Long.parseLong(l.get(j));
+            if (lCandidate + lPair == 2020) {
+               return String.valueOf((lCandidate*lPair));
+            }
+         }
+      }
+      return "No hay solucion";
+   }
+
+   private static String partTwo(List<String> l) {
+      for (int i=0; i < l.size() - 2; i++) {
+         String firstNum = l.get(i);
+         long lFirstNum = Long.parseLong(firstNum);
+         for (int j=i+1; j < l.size() - 1; j++) {
+            long lSecondNum = Long.parseLong(l.get(j));
+            for (String s : l) {
+               long lThirdNum = Long.parseLong(s);
+               if (lFirstNum + lSecondNum + lThirdNum == 2020) {
+                  return String.valueOf((lFirstNum * lSecondNum * lThirdNum));
+               }
+            }
+         }
+      }
+      return "No hay solucion";
+   }
+   
 }
